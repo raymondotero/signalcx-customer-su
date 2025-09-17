43761bcb9a4ffaa8aee680c97fbc86826622a9bc
raymondotero <91905443+raymondotero@users.noreply.github.com>
2025-09-17 23:24:13 +0000

    Edited Spark

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,9 +1,7 @@
 import { ComponentProps } from "react"
-import * as DialogPrimitive from "@radix-ui/react-dialog"
 import XIcon from "lucide-react/dist/esm/icons/x"
-import { cn } from "@/lib/utils"
 
-function Dialog({
+
   ...props
 }: ComponentProps<typeof DialogPrimitive.Root>) {
   return <DialogPrimitive.Root data-slot="dialog" {...props} />
